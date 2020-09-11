b67cb2127c666e716e06c25b58b41f425dd56d17
Emanuel Flores <mufymes@gmail.com>
2020-09-11 16:58:09 -0500

    Reunion template created

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
-   url(r'^', include('general.urls')), 
+   url(r'', include('general.urls')), 
+   url(r'reuniones/', include('reuniones.urls')), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- reuniones/views.py
+++ reuniones/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def reunion(request):
+    return render(request, 'reuniones/reunion.html')
