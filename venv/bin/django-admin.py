9d1f7c1af1627dac2bab112455384c3d85fb0dea
Emanuel Flores <mufymes@gmail.com>
2020-09-11 15:24:48 -0500

    Virtual env deleted

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/emanuel/Documents/WebDevelopment/DjangoProjects/Newcompa-AWS/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
