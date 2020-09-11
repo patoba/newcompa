bd81a31cc5792b2d3ea6a228de75894cae41c0d6
roher1727 <roher1727@gmail.com>
2020-09-11 16:23:09 -0500

    [EDIT] base sin container

--- general/views.py
+++ general/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def general(request):
-    return render(request, 'general/base.html')
+    return render(request, 'general/principal.html')
