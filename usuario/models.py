e3a90771d8c9f6c9053d37d4c2b040d5448c4ee6
patoba <patobarrero@gmail.com>
2020-09-11 21:07:20 -0500

    modelos

--- usuario/models.py
+++ usuario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pregunta(models.Model):
+    pass
+
+class Respuesta(models.Model):
+    pass
+
+class Pregunta_Respuesta_Usuario(models.Model):
+    pass
