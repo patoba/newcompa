66bc3dfc984a7cd61dc4f1096279ab303a731c6d
patoba <patobarrero@gmail.com>
2020-09-11 19:37:32 -0500

    registro y log in

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -18,8 +18,11 @@ from django.conf.urls.static import static
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.urls import path
+
 urlpatterns = [
    url(r'^admin/', admin.site.urls),
    url(r'^', include('general.urls')), 
-#    url(r'^', include('usuario.urls')),
+   url(r'^', include('usuario.urls')),
+   path("", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
